225b420629208068db5a20ff558857c46a3c42e5
martijnfeld <martijnfeld@live.nl>
2020-04-22 17:02:38 +0200

    buefy geinstalleerd

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './plugins/buefy'
+import './scss/main.scss'
 
 Vue.config.productionTip = false
 
